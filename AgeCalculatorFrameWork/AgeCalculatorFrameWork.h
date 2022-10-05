c0745ef8ba8a25df18617f2a5b29910414a79a74
Sarathkumar <27sarathkumar.s@gmail.com>
2022-10-05 19:51:34 +0530

    Initial Commit

---
+++ AgeCalculatorFrameWork/AgeCalculatorFrameWork.h
@@ -0,0 +1,18 @@
+//
+//  AgeCalculatorFrameWork.h
+//  AgeCalculatorFrameWork
+//
+//  Created by SarathKumar S on 05/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AgeCalculatorFrameWork.
+FOUNDATION_EXPORT double AgeCalculatorFrameWorkVersionNumber;
+
+//! Project version string for AgeCalculatorFrameWork.
+FOUNDATION_EXPORT const unsigned char AgeCalculatorFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AgeCalculatorFrameWork/PublicHeader.h>
+
+
